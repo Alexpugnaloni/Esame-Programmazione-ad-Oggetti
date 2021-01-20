a4c3193c8fff8f9aa26f20aead86bdd1e3fef537
TommasoRiva <s1095496@studenti.univpm.it>
2021-01-20 16:04:13 +0100

    added scheduler

--- src/main/java/it/forecast/Openweather/OpenweatherApplication.java
+++ src/main/java/it/forecast/Openweather/OpenweatherApplication.java
@@ -2,8 +2,11 @@ package it.forecast.Openweather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
+@EnableScheduling
 public class OpenweatherApplication {
 
 	public static void main(String[] args) {
